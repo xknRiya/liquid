9585704e180f3b8046d02124185bc24e84b30382
realidad aumentada <xknriya@gmail.com>
2025-05-06 12:31:15 -0300

    untested frontend updates

--- frontend/src/components/Inputs.tsx
+++ frontend/src/components/Inputs.tsx
@@ -1,8 +1,33 @@
 // import { evaluate } from "mathjs";
+import { useState } from "react";
 import { useLiquid } from "../hooks/useLiquid";
+
+interface InputContainerProps {
+    id: string;
+    label: string;
+    value: string | number;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    children?: React.ReactNode;
+    type?: string;
+    placeholder?: string;
+    ref?: React.RefObject<HTMLInputElement | null>;
+    [key: string]: any;
+}
+
+export const InputContainer: React.FC<InputContainerProps> = ({ label, id, value, onChange, children, type, placeholder, ref, ...props }) => {
+    return (
+        <div id={id}>
+            <label htmlFor={id}>{label}</label>
+            <input ref={ref} type={type || 'text'} name={id} value={value} autoComplete='off' onChange={onChange} placeholder={placeholder || 'Some text'} {...props} />
+            {children}
+        </div>
+    );
+};
 
 export const Inputs: React.FC = () => {
     const { inputRef, datalistRef, inputValue, setInputValue, employees, remunerations, selectedRemuneration, setSelectedRemuneration } = useLiquid();
+
+    const [period, setPeriod] = useState({ month: 3, year: 2025 });
 
     const inputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.value === '') return setInputValue('');
@@ -39,23 +64,43 @@ export const Inputs: React.FC = () => {
     return (
         <div id='inputs'>
             <div className='inputs-option'>
-                <label htmlFor='employee-file-number'>Ingrese el N° Legajo: </label>
-                <input ref={inputRef} list='' autoComplete='off' id='employee-file-number' placeholder='1000, 1001, 1002...' value={inputValue} onChange={inputOnChange} />
-                <datalist ref={datalistRef} id='employees-datalist' >
-                    {
-                        employees.map((employee, index) =>
-                            <option key={index} value={employee.legajo} onClick={() => setInputValue(employee.legajo)}>
-                                {employee.legajo} - {employee.apellido}, {employee.nombre}
-                            </option>
-                        )
-                    }
-                </datalist>
+                <InputContainer
+                    label="N° Legajo: "
+                    ref={inputRef}
+                    id='employee-file-number'
+                    value={inputValue}
+                    onChange={inputOnChange}
+                    placeholder="1000, 1001, 1002...">
+                    <datalist ref={datalistRef} id='employees-datalist' >
+                        {
+                            employees.map((employee, index) =>
+                                <option key={index} value={employee.legajo} onClick={() => setInputValue(employee.legajo)}>
+                                    {employee.legajo} - {employee.apellido}, {employee.nombre}
+                                </option>
+                            )
+                        }
+                    </datalist>
+                </InputContainer>
+                {/* <label htmlFor='employee-file-number'>Ingrese el N° Legajo: </label> */}
+                {/* <input ref={inputRef} list='' autoComplete='off' id='employee-file-number' placeholder='1000, 1001, 1002...' value={inputValue} onChange={inputOnChange} /> */}
+
             </div>
 
             <div className="inputs-option remunerations">
-                {/* periodo */}
-                <label htmlFor='remunerations-period'>Seleccione el periodo: </label>
-                <input type="text" id="remunerations-period" name="remunerations-period" value="2024" onChange={() => { }} />
+                <div id="remunerations-period">
+                    <label htmlFor='remunerations-period'>Período: </label>
+                    <input type="text" id="remunerations-period-month" name="remunerations-period-month" value={period.month} onChange={(event) => {
+                        const newPeriod = { month: parseInt(event.target.value) || 0, year: period.year };
+                        if (newPeriod.month > 12) return;
+                        setPeriod(newPeriod)
+                    }} />
+                    <span> / </span>
+                    <input type="text" id="remunerations-period-year" name="remunerations-period-year" value={period.year} onChange={(event) => {
+                        const newPeriod = { month: period.month, year: parseInt(event.target.value) || 0 };
+                        if (newPeriod.year < 0 || newPeriod.year > (new Date()).getFullYear()) return;
+                        setPeriod(newPeriod)
+                    }} />
+                </div>
                 <label htmlFor='remunerations-select'>Seleccione la remuneración: </label>
                 <select name='remunerations' id='remunerations-select' className='inputs-option' onChange={remunerationsOnChange}>
                     <option value='none'>---</option>
@@ -75,16 +120,20 @@ export const Inputs: React.FC = () => {
                 {
                     selectedRemuneration && (
                         <>
-                            <label htmlFor="remuneration-units">Unidades: </label>
+                            <InputContainer label="Unidades: " id="remuneration-units" value={selectedRemuneration.unidades} onChange={selectedRemunerationUnitsOnChange} type="number">
+                                <span>{selectedRemuneration.tipo_unidad}</span>
+                            </InputContainer>
+                            {/* <label htmlFor="remuneration-units">Unidades: </label>
                             <div>
-
                                 <input type="number" id="remuneration-units" name="remuneration-units" value={selectedRemuneration.unidades} onChange={selectedRemunerationUnitsOnChange} />
                                 <span>{selectedRemuneration.tipo_unidad}</span>
-                            </div>
-                            <label htmlFor="remuneration-base-value">Valor base: </label>
-                            <input type="number" id="remuneration-base-value" name="remuneration-base-value" value={selectedRemuneration.valor} onChange={() => { }} disabled />
-                            <label htmlFor="remuneration-value">Valor: </label>
-                            <input type="number" id="remuneration-value" name="remuneration-value" value={selectedRemuneration.valor * selectedRemuneration.unidades} onChange={() => { }} />
+                            </div> */}
+                            <InputContainer label="Valor base: " id="remuneration-base-value" value={selectedRemuneration.valor} onChange={() => { }} disabled></InputContainer>
+                            {/* <label htmlFor="remuneration-base-value">Valor base: </label>
+                            <input type="number" id="remuneration-base-value" name="remuneration-base-value" value={selectedRemuneration.valor} onChange={() => { }} disabled /> */}
+                            <InputContainer label="Valor: " id="remuneration-value" value={selectedRemuneration.valor * selectedRemuneration.unidades} onChange={() => { }} disabled></InputContainer>
+                            {/* <label htmlFor="remuneration-value">Valor: </label>
+                            <input type="number" id="remuneration-value" name="remuneration-value" value={selectedRemuneration.valor * selectedRemuneration.unidades} onChange={() => { }} /> */}
                         </>
                     )
                 }
